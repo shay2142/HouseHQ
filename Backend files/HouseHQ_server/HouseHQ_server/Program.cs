5ea534292b84b5dc5c4c17f11815491e9d0bae5a
שי כהן <shay53769@gmail.com>
2021-03-26 05:56:45 +0300

    Connecting http server to HouseHQ_server

--- Backend files/HouseHQ_server/HouseHQ_server/Program.cs
+++ Backend files/HouseHQ_server/HouseHQ_server/Program.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Data.SQLite;
+using System.Threading;
+
+using jsonDeserialize;
+using jsonSerializer;
+using dataBase;
 
 namespace HouseHQ_server
 {
@@ -14,9 +27,13 @@ namespace HouseHQ_server
         [STAThread]
         static void Main()
         {
+            httpServer server2 = new httpServer();
+            Thread thr = new Thread(new ThreadStart(server2.runServer));
+            thr.Start();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
---
+++ Backend files/HouseHQ_server/HouseHQ_server/jsonDeserialize.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jsonDeserialize
+{
+    class login
+    {
+        // Make sure all class attributes have relevant getter setter.
+        public string name { get; set; }
+
+        public string password { get; set; }
+    }
+    class singup
+    {
+        public string name { get; set; }
+
+        public string password { get; set; }
+
+        public string mail { get; set; }
+
+        public string key { get; set; }
+    }
+    class addAppForUser
+    {
+        public string userName { get; set; }
+        public string appName { get; set; }
+    }
+    class deleteUser
+    {
+        public string userNameDelete { get; set; }
+        public string adminUserName { get; set; }
+    }
+    class changeAccount
+    {
+        public string userName { get; set; }
+        public string oldPassword { get; set; }
+        public string newPassword { get; set; }
+        public string mail { get; set; }
+        public int admin { get; set; }
+    }
+    class deleteAppFromUser
+    {
+        public string userName { get; set; }
+        public string appName { get; set; }
+    }
+}
