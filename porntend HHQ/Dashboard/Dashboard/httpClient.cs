ec2057ba83cacd9501c265dd7f536a31aaeecb2f
שי כהן <shay53769@gmail.com>
2021-04-08 09:54:43 +0300

    Arranging the code

--- porntend HHQ/Dashboard/Dashboard/httpClient.cs
+++ porntend HHQ/Dashboard/Dashboard/httpClient.cs
@@ -33,6 +33,7 @@ namespace HTTP_CLIENT
             }
             return ip;
         }
+
         public string sent(string json, string ip, string code)
         {
             string result = "";
@@ -47,117 +48,18 @@ namespace HTTP_CLIENT
             }
             return result;
         }
-        //error
+
         public async Task<string> msg(string json, string ip, string code)
         {
             var data = new StringContent(code + "&" + json, Encoding.UTF8, "application/json");
 
             var url = "http://" + ip + ":8080/";
-            /*using*/
+
             var client = new HttpClient();
 
             var response = await client.PostAsync(url, data);
 
             return response.Content.ReadAsStringAsync().Result;
         }
-    }
-    class okLogin
-    {
-        // Make sure all class attributes have relevant getter setter.
-
-        public string name { get; set; }
-
-        public string mail { get; set; }
-
-        public List<string> appList { get; set; }
-
-        public string key { get; set; }
-
-        public string img { get; set; }
-    }
-    class okSingup
-    {
-        // Make sure all class attributes have relevant getter setter.
-
-        public bool ok { get; set; }
-    }
-    class login
-    {
-        // Make sure all class attributes have relevant getter setter.
-        public string name { get; set; }
-
-        public string password { get; set; }
-    }
-    class singup
-    {
-        public string name { get; set; }
-
-        public string password { get; set; }
-
-        public string mail { get; set; }
-
-        public string key { get; set; }
-    }
-    class error
-    {
-        public string msg { get; set; }
-    }
-    class getAllUsers
-    {
-        public List<string> usersList { get; set; }
-    }
-    class userInformation
-    {
-        public string password { get; set; }
-        public string mail { get; set; }
-        public string key { get; set; }
-    }
-    class getUserInformation
-    {
-        public string userName { get; set; }
-    }
-    class changeAccount
-    {
-        public string userName { get; set; }
-        public string oldPassword { get; set; }
-        public string newPassword { get; set; }
-        public string mail { get; set; }
-        public string level { get; set; }
-    }
-    public class getDB
-    {
-        public int ID { get; set; }
-        public string userName { get; set; }
-        public string password { get; set; }
-        public string mail { get; set; }
-        public string LEVEL_KEY { get; set; }
-        public string STATUS { get; set; }
-    }
-    class jsonSentDB
-    {
-        public List<getDB> db { get; set; }
-    }
-    class getAllApps
-    {
-        public List<string> allAppList { get; set; }
-    }
-    class addAppForUser
-    {
-        public string userName { get; set; }
-        public string appName { get; set; }
-    }
-    class deleteAppFromUser
-    {
-        public string userName { get; set; }
-        public string appName { get; set; }
-    }
-    class deleteUser
-    {
-        public string userNameDelete { get; set; }
-        public string adminUserName { get; set; }
-    }
-    class logoutUser
-    {
-        public string userName { get; set; }
     }
 }
