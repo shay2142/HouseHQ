f063d830a4cb2b070ba7e71b11235978d9c7b196
שי כהן <shay53769@gmail.com>
2021-05-04 00:44:04 +0300

    update remote app management

--- HouseHQ_server/HouseHQ_server/remoteApp Management.cs
+++ HouseHQ_server/HouseHQ_server/remoteApp Management.cs
@@ -3,10 +3,159 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cassia;
 
 namespace HouseHQ_server
 {
     class remoteApp_Management
     {
+        public void killProcess(List<string> namePc, List<string> processToKill)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                while (true)
+                {
+                    foreach (ITerminalServicesSession session in server.GetSessions())
+                    {
+                        if (namePc.Contains(session.ClientName))
+                        {
+                            foreach (var proc in session.GetProcesses())
+                            {
+                                if (processToKill.Contains(proc.ProcessName))
+                                {
+                                    try
+                                    {
+                                        proc.Kill();
+                                    }
+                                    catch
+                                    {
+                                        killProcess(namePc, processToKill);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+        public int getSession(string namePc)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.ClientName == namePc && session.ClientName.ToString() != "")
+                    {
+                        return session.SessionId;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public void sentMsg(int sessionId, string msg)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.SessionId == sessionId && session.ClientName.ToString() != "")
+                    {
+                        session.MessageBox(msg);
+                    }
+                }
+            }
+        }
+
+        public void logOff(int sessionId)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.SessionId == sessionId && session.ClientName.ToString() != "")
+                    {
+                        session.Logoff();
+                    }
+                }
+            }
+        }
+
+        public bool theUserIsConnect(int sessionId)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.SessionId == sessionId && session.ClientName.ToString() != "")
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public string getLastInputTime(int sessionId)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.SessionId == sessionId && session.ClientName.ToString() != "")
+                    {
+                        return session.LastInputTime.ToString();
+                    }
+                }
+            }
+            return "";
+        }
+
+        public void logOffAllUsers()
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.ClientName.ToString() != "")
+                    {
+                        session.Logoff();
+                    }
+                }
+            }
+        }
+
+        public List<string> getAllClientName()
+        {
+            List<string> ans = new List<string>();
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                {
+                    if (session.ClientName.ToString() != "")
+                    {
+                        ans.Add(session.ClientDisplay.ToString());
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
--- HouseHQ_server/HouseHQ_server/remoteApp.cs
+++ HouseHQ_server/HouseHQ_server/remoteApp.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Microsoft.Win32;
+using Cassia;
 
 namespace HouseHQ_server
 {
