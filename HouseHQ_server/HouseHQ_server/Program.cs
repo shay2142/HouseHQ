b483c701e642d39aba89c8e085dc374b9f82d964
שי כהן <shay53769@gmail.com>
2021-06-23 11:22:42 +0300

    add rdpwrap

--- HouseHQ_server/HouseHQ_server/Program.cs
+++ HouseHQ_server/HouseHQ_server/Program.cs
@@ -28,6 +28,9 @@ namespace HouseHQ_server
         [STAThread]
         static void Main()
         {
+            rdpwrap rdp = new rdpwrap();
+            rdp.update();
+
             upnp upnp = new upnp();
             upnp.createUpnp();
 
